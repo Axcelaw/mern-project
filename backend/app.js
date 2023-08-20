5077b647602eefc9feb9c7d1e9c4908da860bf70
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-08-20 11:43:07 -0300

    Created MongoDB connection using Mongoose.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected successfully to database."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
@@ -9,5 +19,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}.`);
 });
